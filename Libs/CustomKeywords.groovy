d628413a55f96bd0751f534e32bff0692093d994
kaewsirinice <58505285+kaewsirinice@users.noreply.github.com>
2021-03-12 11:44:51 +0700

    Version 1.0.0

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,29 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+def static "com.fb180.utilities.MyUtilities.myCheckbox1"(
+    	TestObject obj	
+     , 	String option	) {
+    (new com.fb180.utilities.MyUtilities()).myCheckbox1(
+        	obj
+         , 	option)
+}
+
+def static "com.fb180.utilities.MyUtilities.myCheckbox2"(
+    	String option1	
+     , 	int to	
+     , 	TestObject obj2	
+     , 	String option2	) {
+    (new com.fb180.utilities.MyUtilities()).myCheckbox2(
+        	option1
+         , 	to
+         , 	obj2
+         , 	option2)
+}
